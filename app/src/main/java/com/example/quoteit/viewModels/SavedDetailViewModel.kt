f9d9facbc5799d52c3e6adb2b047b7d8ff354cf0
SR PRAWIN RAJA <srprawinraja@gmail.com>
2025-12-04 21:16:30 +0530

    fixed viewmodel bug

--- app/src/main/java/com/example/quoteit/viewModels/SavedDetailViewModel.kt
+++ app/src/main/java/com/example/quoteit/viewModels/SavedDetailViewModel.kt
@@ -1,8 +1,8 @@
 package com.example.quoteit.viewModels
 
+import androidx.lifecycle.ViewModel
 import com.example.quoteit.db.saved.SavedQuoteRepository
 
-class SavedDetailViewModel (savedQuoteRepository: SavedQuoteRepository){
+class SavedDetailViewModel (savedQuoteRepository: SavedQuoteRepository): ViewModel(){
     val savedQuoteFlow = savedQuoteRepository.savedQuoteFlow
-
 }
--- app/src/main/java/com/example/quoteit/viewModels/SavedViewModel.kt
+++ app/src/main/java/com/example/quoteit/viewModels/SavedViewModel.kt
@@ -1,8 +1,9 @@
 package com.example.quoteit.viewModels
 
+import androidx.lifecycle.ViewModel
 import com.example.quoteit.db.saved.SavedQuoteRepository
 
-class SavedViewModel (savedQuoteRepository: SavedQuoteRepository){
+class SavedViewModel (savedQuoteRepository: SavedQuoteRepository): ViewModel() {
     val savedQuoteFlow = savedQuoteRepository.savedQuoteFlow
 
 }
